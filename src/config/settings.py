3f534d5957dcc7d164eb2a752417c915b63c7197
fayzikuloff.b <fayzikuloff.b@gmail.com>
2023-01-14 13:36:51 +0300

    Simple change db env namebling

--- src/config/settings.py
+++ src/config/settings.py
@@ -80,9 +80,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
         "HOST": os.environ.get("DB_HOST"),
         "PORT": int(os.environ.get("DB_PORT")),
     }
